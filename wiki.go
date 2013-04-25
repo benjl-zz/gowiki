4da31e92d37051ed57cad7d05045274bc4ea203c
Ben <benjamin.jl.cox@gmail.com>
2013-04-25 12:38:22 -0400

    Error handling

--- wiki.go
+++ wiki.go
@@ -25,12 +25,16 @@ func loadPage(title string) (*Page, error){
 	return &Page{Title: title, Body: body}, nil
 }
 
-const lenPath = len("/view/")
+var templates = tempalte.Must(template.ParseFiles("edit.html", "view.html"))
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page){
-	t, _ := template.ParseFiles(tmpl + "html")
-	t.Execute(w, p)
+	err = tempaltes.ExecuteTemplate(w, tmpl+".html", p)
+	if err != nil{
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
+
+const lenPath = len("/view/")
 
 func viewHandler(w http.ResponseWriter, r *http.Request){
 	title := r.URL.Path[len.Path:]
@@ -49,6 +53,18 @@ func editHandler(w http.ResponseWriter, r *http.Request){
 		p = &Page(Title: title)
 	}
 	renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request){
+	title := r.URL.Path[lenPath:]
+	body := r.FormValue("body")
+	p := &Page{Title:, title, Body: []byte(body)}
+	err = p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w,r,"view"+title, http.StatusFound)
 }
 
 func main(){
